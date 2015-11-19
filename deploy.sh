b5084e940631b4cfb8a653715d025ca5a4c4f544
Willem Mali <willem.mali@gmail.com>
2015-11-19 09:51:02 +0100

    made script verbose and prompt for confirmation

--- deploy.sh
+++ deploy.sh
@@ -6,34 +6,43 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+set -x
+homedir="/home/willem"
+dir=$homedir/dotfiles                    # dotfiles directory
 olddir=$dir/old             # old dotfiles backup directory
 files="bashrc bash_aliases bash_logout vimrc vim Xdefaults profile"    # list of files/folders to symlink in homedir
+
+# read input
+read -p "Are you sure you want to install dotfiles with these settings? Y/N: " answer
+
+# handle input
+while true
+do
+    case $answer in
+        [yY]* ) break;;
+        [nN]* ) exit;;
+        * )     read -p "Y/N: " answer;;
+    esac
+done
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    if [ -h ~/.$file ]; then
-        echo "Skipping $file because symlink already exists"
+    if [ -h $homedir/.$file ]; then
+        echo "symlink already exists"
         continue
     fi
-    if [ -e ~/.$file ]; then
-        echo "Moving .$file to $olddir"
-        mv ~/.$file $olddir
+    if [ -e $homedir/.$file ]; then
+        mv $homedir/.$file $olddir
     fi
     if [ -e $dir/$file ]; then
-        echo "Creating symlink to $file in home directory."
-        ln -s $dir/$file ~/.$file
+        ln -s $dir/$file $homedir/.$file
     fi
 done
